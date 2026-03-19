3d8c8e035c70e93ab86171daf4de0d2a7be84aee
Luna <matthew.willming@gmail.com>
2026-03-19 09:47:22 -0500

    Add files via upload

---
+++ BattleshipLunaFunny/Ship.java
@@ -0,0 +1,18 @@
+public class Ship {
+    private int length;
+    private String name;
+
+
+    public Ship(int l, String n){
+        length = l;
+        name = n;
+    }
+    public int getLength(){
+        return length;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
